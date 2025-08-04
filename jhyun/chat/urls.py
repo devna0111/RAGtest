3217cd7cc36172d66f5bd28212b6aa833e36395e
devna0111 <devna0111@gmail.com>
2025-08-04 17:42:38 +0900

    modify

--- jhyun/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chat_page, name='chat_page'),
-    path('upload/', views.file_upload, name='file_upload'),
-]
--- jhyun/presentation/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def presentation_analysis(request):
-    return render(request, 'presentation/analysis.html')
